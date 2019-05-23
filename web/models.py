61bc6f0c227834ea5a60b862cb2cdb8e8ef6f9b9
PSS1998 <p_sadri_s@yahoo.com>
2019-05-23 17:01:02 +0430

    Changed view to json format

--- web/models.py
+++ web/models.py
@@ -4,9 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __unicode__(self):
+        return "{}_token".format(self.user)
+
 class Account(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.username
--- web/views.py
+++ web/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def submit_account(request):
     '''user submits an account information'''
-    print("we are here")
+    print("i am in submit account")
+    print(request.POST)
+
+    return JsonResponse({ 'status': 'ok', }, encoder=JSONEncoder)
