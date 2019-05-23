2bbe0d70922d1d5aa86fa3abade122d651755fea
PSS1998 <p_sadri_s@yahoo.com>
2019-05-24 01:05:04 +0430

    Add template for index and register and register logic

--- web/models.py
+++ web/models.py
@@ -1,9 +1,17 @@
 from __future__ import unicode_literals
-
+from django.utils.crypto import get_random_string
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)  # TODO: do not save password
+
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=48)
--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^submit/account/$', views.submit_account, name='submit_account')
+    url(r'^submit/account/$', views.submit_account, name='submit_account'),
+    url(r'^edit/account/?$', views.edit_account, name='edit_account'),
+    url(r'^accounts/register/?$', views.register, name='register'),
+    url(r'^$', views.index, name='index'),
 ]
