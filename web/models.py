94332b6400a48268aa843d6ae98c55f7ab05e317
PSS1998 <p_sadri_s@yahoo.com>
2019-05-23 15:59:03 +0430

    Django add first model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
