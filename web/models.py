c8776968318cb5f71f73a4ae1eb445cb8d4adc1c
PSS1998 <p_sadri_s@yahoo.com>
2019-05-23 16:16:49 +0430

    Django add uniqe UI to model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,3 +8,5 @@ class Account(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.username
